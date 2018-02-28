5a2f9cb3cab527ff781c31e7f4204d4fa959f6d7
Alexander Zelenyak <zzz.sochi@gmail.com>
2018-03-01 00:03:07 +0300

    Add --allow-subset-of-names to certbot

--- script/le.sh
+++ script/le.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+set -e
+
 if [ "$LETSENCRYPT" = "true" ]; then
-    certbot certonly -t -n --agree-tos --renew-by-default --email "${LE_EMAIL}" --webroot -w /usr/share/nginx/html -d ${LE_FQDN}
+    certbot certonly -t -n --agree-tos --renew-by-default --allow-subset-of-names --email "${LE_EMAIL}" --webroot -w /usr/share/nginx/html -d ${LE_FQDN}
     FIRST_FQDN=$(echo "$LE_FQDN" | cut -d"," -f1)
     cp -fv /etc/letsencrypt/live/${FIRST_FQDN}/privkey.pem /etc/nginx/ssl/le-key.pem
     cp -fv /etc/letsencrypt/live/${FIRST_FQDN}/fullchain.pem /etc/nginx/ssl/le-crt.pem
